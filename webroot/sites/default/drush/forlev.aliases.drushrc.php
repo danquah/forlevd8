553e675938a3408d8cdb99649d9757714f7ddac3
Mads Høgstedt Danquah <mads@danquah.dk>
2016-02-27 21:54:52 +0100

    Add production drush-alis

---
+++ webroot/sites/default/drush/forlev.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases['prod'] = array(
+  'remote-host' => 'ny.flab.dk',
+  'remote-user' => 'forlevdk_login',
+  'uri' => 'dev.forlev.dk',
+  'root' => '/home/hostroot/sites/danquah/forlevdk/forlevd8/webroot',
+  'path-aliases' => array(
+    '%drush-script' => '/usr/local/bin/drush',
+  ),
+);

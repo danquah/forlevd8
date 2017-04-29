708464c952dea8d43e93def1c02f0bc2fefd6951
Mads H. Danquah <mads@danquah.dk>
2017-04-29 10:05:17 +0200

    Dinghy related permission-fix

--- scripts/site-reset.sh
+++ scripts/site-reset.sh
@@ -9,12 +9,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Chmod to 777 if the file is not owned by www-data
 cd "${SCRIPT_DIR}/../"
-find web/sites/default/files \! -uid 33  \! -print0 -name .gitkeep | sudo xargs -0 chmod 777
+find web/sites/default/files \! -user $USER  \! -print0 -name .gitkeep | sudo xargs -0 chmod 777
 
 # Make sites/default read-only and executable
 sudo chmod 555 web/sites/default
 
-time docker-compose exec fpm sh -c "\
+time docker-compose exec --user www-data fpm sh -c "\
   cd /var/www/web && \
   echo 'Importing configuration' && \
   drush cim --preview=diff -y && \
